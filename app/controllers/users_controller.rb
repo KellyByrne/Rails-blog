858cdba8f45696aee2f548cb8d4f28f9cd959fed
Kelly Bennett <kellyannbennett91@gmail.com>
2024-01-29 14:18:35 -0500

    add users create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to KB Blog #{@user.username}, you have signed up successfully"
+      redirect_to articles_path
+    else 
+      render 'new'
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
 end
